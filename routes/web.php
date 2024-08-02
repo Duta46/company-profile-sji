883ff03dde725bd23a032e1acc5786217247296d
Duta Sby <ykoplo17@gmail.com>
2024-08-02 08:55:18 +0700

    Layoting dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/admin/login', [AuthController::class, 'index'])->name('admin.login');
+Route::post('/admin/login', [AuthController::class, 'login'])->name('admin.login.post');
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified', ])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/change-password', [ProfileController::class, 'change_password'])->name('changePassword');
+    Route::post('/update-password', [ProfileController::class, 'update_password'])->name('updatePassword');
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('admin.logout');
 });
